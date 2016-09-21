a287f3d176500ced1198861f5f4ad734f8158bfe
Rik Meijer <ha.meijer@avans.nl>
2016-09-21 15:00:13 +0200

    fixed 'Second View'

--- MusicCollectionMVVMLight/ViewModel/SongListViewModel.cs
+++ MusicCollectionMVVMLight/ViewModel/SongListViewModel.cs
@@ -35,26 +35,80 @@ namespace MusicCollectionMVVMLight.ViewModel
         //Commands
         public ICommand ShowAddSongCommand { get; set; }
 
-        //Commands
+        public ICommand PlayalongCommand { get; set; }
+
         public ICommand DeleteSongCommand { get; set; }
+
+
+        public ICommand PrevCommand { get; set; }
+        public ICommand NextCommand { get; set; }
 
         public SongListViewModel()
         {
             songRepository = new DummySongRepository();
             var songList = songRepository.GetSongs().Select(s => new SongViewModel(s));
             ShowAddSongCommand = new RelayCommand(ShowAddSong, CanShowAddSong);
+            PlayalongCommand = new RelayCommand(Playalong, CanPlayalong);
             DeleteSongCommand = new RelayCommand(DeleteSong, CanDeleteSong);
+
+            PrevCommand = new RelayCommand(Prev, CanPrevSong);
+            NextCommand = new RelayCommand(Next, CanNextSong);
             Songs = new ObservableCollection<SongViewModel>(songList);
+        }
+
+        private void Next()
+        {
+            if (SelectedSong == Songs.Last())
+            {
+                SelectedSong = Songs.First();
+            }
+            else
+            {
+                SelectedSong = Songs.ElementAt(Songs.IndexOf(_selectedSong) + 1);
+            }
+        }
+
+        private bool CanNextSong()
+        {
+            return Songs.Count > 0;
+        }
+
+        private void Prev()
+        {
+            if (SelectedSong == Songs.First())
+            {
+                SelectedSong = Songs.Last();
+            }
+            else
+            {
+                SelectedSong = Songs.ElementAt(Songs.IndexOf(_selectedSong) - 1);
+            }
+        }
+
+        private bool CanPrevSong()
+        {
+            return Songs.Count > 0;
+        }
+
+        private void Playalong()
+        {
+            PlayalongWindow _playalongWindow = new PlayalongWindow();
+            _playalongWindow.Show();
+        }
+
+        private bool CanPlayalong()
+        {
+            return SelectedSong != null;
         }
 
         private bool CanDeleteSong()
         {
-            return Songs.Contains(_selectedSong);
+            return Songs.Contains(SelectedSong);
         }
 
         private void DeleteSong()
         {
-            Songs.Remove(_selectedSong);
+            Songs.Remove(SelectedSong);
         }
 
         public void ShowAddSong()
